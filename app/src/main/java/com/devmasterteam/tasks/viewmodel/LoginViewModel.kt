3a0f30b02e90bc72289d8ef0d769b8cac71fa825
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-07-17 09:38:31 -0300

    Exclusão dos métodos que utilizavam a API na LoginViewModel.kt

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -5,10 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.devmasterteam.tasks.service.constants.TaskConstants
-import com.devmasterteam.tasks.service.listener.APIListener
-import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.model.ValidationModel
-import com.devmasterteam.tasks.service.repository.PersonRepository
 import com.devmasterteam.tasks.service.repository.SecurityPreferences
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -17,7 +14,6 @@ import kotlinx.coroutines.flow.asStateFlow
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     // Instancias
-    private val personRepository = PersonRepository(application.applicationContext)
     private val securityPreferences = SecurityPreferences(application.applicationContext)
   //   private val priorityRepository = PriorityRepository(application.applicationContext)
 
@@ -28,22 +24,6 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     private val _loggedUser = MutableStateFlow(true)
     val loggedUser = _loggedUser.asStateFlow()
 
-    // Faz login usando API
-    fun doLogin(email: String, password: String) {
-        personRepository.login(email, password, object : APIListener<PersonModel> {
-            override fun onSuccess(result: PersonModel) {
-                securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, result.token)
-                securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, result.personKey)
-                securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, result.name)
-                RetrofitClient.addHeaders(result.token, result.personKey)
-                _login.value = ValidationModel()
-            }
-
-            override fun onFailure(message: String) {
-                _login.value = ValidationModel(message)
-            }
-        })
-    }
 
     // Verifica se usuário está logado
     fun verifyLoggedUser() {
@@ -54,16 +34,5 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
         val logged = (token != "" && person != "")
         _loggedUser.value = logged
 
-//        if (!logged) {
-//            priorityRepository.list(object : APIListener<List<PriorityModel>> {
-//                override fun onSuccess(result: List<PriorityModel>) {
-//                    priorityRepository.save(result)
-//                }
-//
-//                override fun onFailure(message: String) {
-//                    val s = message
-//                }
-//            })
-//        }
     }
 }
