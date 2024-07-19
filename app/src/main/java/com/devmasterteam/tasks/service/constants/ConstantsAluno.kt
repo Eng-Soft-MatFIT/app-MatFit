996d0cfdb7c13f31320ced52f2276b5d1c6b6e50
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-07-19 15:47:31 -0300

    Renomeado o arquivo de constantes do aluno

---
+++ app/src/main/java/com/devmasterteam/tasks/service/constants/ConstantsAluno.kt
@@ -0,0 +1,12 @@
+package com.devmasterteam.tasks.service.constants
+
+class ConstantsAluno private constructor() {
+
+    object AttributosAluno {
+        const val CPF = "cpf"
+        const val NAME = "nome"
+        const val SPORT = "esporte"
+        const val DAY = "dataDePagamento"
+    }
+
+}
