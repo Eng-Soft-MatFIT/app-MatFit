53b478aff791217df8a435fa52520f0e9e9f6052
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-07-22 20:40:46 -0300

    adicionando o binding e inflando o layout

--- app/src/main/java/com/devmasterteam/tasks/view/ui/funcionarios/FuncionariosFragment.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/ui/funcionarios/FuncionariosFragment.kt
@@ -1,10 +1,23 @@
 package com.devmasterteam.tasks.view.ui.funcionarios
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.devmasterteam.tasks.databinding.FragmentFuncionariosBinding
 
 class FuncionariosFragment : Fragment() {
 
+    private lateinit var binding: FragmentFuncionariosBinding
 
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?, ): View {
+
+        binding = FragmentFuncionariosBinding.inflate(inflater, container, false)
+
+
+        return binding.root
+    }
 
 
 }
