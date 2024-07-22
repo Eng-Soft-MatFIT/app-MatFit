3a22c26aef02ddd45e589576e01873d8423c33fe
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-07-22 11:45:46 -0300

    movido para o pacote ui

---
+++ app/src/main/java/com/devmasterteam/tasks/view/ui/viewmodel/AlunoViewModel.kt
@@ -0,0 +1,10 @@
+package com.devmasterteam.tasks.view.ui.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class AlunoViewModel(application: Application) : AndroidViewModel(application) {
+
+
+
+}
