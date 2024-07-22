3860b4be7554396d1a94f862c2f9ee341cef99e4
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-07-22 20:40:40 -0300

    adicionando o binding e inflando o layout

--- app/src/main/java/com/devmasterteam/tasks/view/ui/equipamentos/EquipamentosFragment.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/ui/equipamentos/EquipamentosFragment.kt
@@ -1,8 +1,21 @@
 package com.devmasterteam.tasks.view.ui.equipamentos
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.devmasterteam.tasks.databinding.FragmentEquipamentosBinding
 
 class EquipamentosFragment : Fragment(){
+
+    private lateinit var binding: FragmentEquipamentosBinding
+
+    override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View {
+        binding =FragmentEquipamentosBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
 
 
 
