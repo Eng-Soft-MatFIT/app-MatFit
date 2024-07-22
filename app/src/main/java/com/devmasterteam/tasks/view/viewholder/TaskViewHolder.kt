d53554a2e620cdd109ced140d445386acdb247f6
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-07-22 11:36:21 -0300

    Comentado a ViewHolder

--- app/src/main/java/com/devmasterteam/tasks/view/viewholder/TaskViewHolder.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/viewholder/TaskViewHolder.kt
@@ -1,23 +1,23 @@
 package com.devmasterteam.tasks.view.viewholder
 
-import android.annotation.SuppressLint
-import android.app.AlertDialog
-import androidx.recyclerview.widget.RecyclerView
-import com.devmasterteam.tasks.R
-import com.devmasterteam.tasks.databinding.RowTaskListBinding
-import com.devmasterteam.tasks.service.listener.TaskListener
-import com.devmasterteam.tasks.service.model.TaskModel
-import java.text.SimpleDateFormat
-
-class TaskViewHolder(private val itemBinding: RowTaskListBinding, val listener: TaskListener) :
-    RecyclerView.ViewHolder(itemBinding.root) {
-
-    /**
-     * Atribui valores aos elementos de interface e também eventos
-     */
-    @SuppressLint("SimpleDateFormat")
-    fun bindData(task: TaskModel) {
-
-
-    }
-}
+//import android.annotation.SuppressLint
+//import android.app.AlertDialog
+//import androidx.recyclerview.widget.RecyclerView
+//import com.devmasterteam.tasks.R
+//import com.devmasterteam.tasks.databinding.RowTaskListBinding
+//import com.devmasterteam.tasks.service.listener.TaskListener
+//import com.devmasterteam.tasks.service.model.TaskModel
+//import java.text.SimpleDateFormat
+//
+//class TaskViewHolder(private val itemBinding: RowTaskListBinding, val listener: TaskListener) :
+//    RecyclerView.ViewHolder(itemBinding.root) {
+//
+//    /**
+//     * Atribui valores aos elementos de interface e também eventos
+//     */
+//    @SuppressLint("SimpleDateFormat")
+//    fun bindData(task: TaskModel) {
+//
+//
+//    }
+// }
