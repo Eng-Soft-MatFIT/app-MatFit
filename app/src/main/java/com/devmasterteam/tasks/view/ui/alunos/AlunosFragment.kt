5ca25cb67b88327dc664af29b71f879dfc8e7bd0
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-07-30 20:07:51 -0300

    criado observers e adicionado a listagem

--- app/src/main/java/com/devmasterteam/tasks/view/ui/alunos/AlunosFragment.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/ui/alunos/AlunosFragment.kt
@@ -5,21 +5,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.databinding.FragmentAlunosBinding
+import com.devmasterteam.tasks.service.constants.Constants
 import com.devmasterteam.tasks.service.listener.OnAlunoListener
+import com.devmasterteam.tasks.service.model.Aluno
 import com.devmasterteam.tasks.view.adapter.AlunoAdapter
 import com.devmasterteam.tasks.view.ui.viewmodel.AlunoViewModel
+import dev.jvitor.gerenciadordematriculas.view.UpdateAlunoActivity
 
 class AlunosFragment : Fragment() {
 
     private lateinit var viewModel: AlunoViewModel
     private lateinit var binding: FragmentAlunosBinding
     private val adapter = AlunoAdapter()
+    private lateinit var aluno: Aluno
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, b: Bundle?): View {
 
@@ -33,15 +38,15 @@ class AlunosFragment : Fragment() {
 
         clickable()
 
-        val listener = object : OnAlunoListener{
+        viewModel.listAllAlunos()
+
+        val listener = object : OnAlunoListener {
             override fun onUpdate(cpf: String) {
-             //   val intent = Intent(context, UpdateAlunoActivity::class.java)
-              //  val aluno = viewModel.get(cpf)
-//                intent.putExtra(Constants.Attributs.CPF, aluno.cpf)
-//                intent.putExtra(Constants.Attributs.NAME, aluno.name)
-//                intent.putExtra(Constants.Attributs.SPORT, aluno.sport)
-//                intent.putExtra(Constants.Attributs.DAY, aluno.day)
-//                startActivity(intent)
+                val intent = Intent(context, UpdateAlunoActivity::class.java)
+                intent.putExtra(Constants.Attributs.CPF, aluno.cpf)
+                intent.putExtra(Constants.Attributs.NAME, aluno.name)
+                intent.putExtra(Constants.Attributs.SPORT, aluno.sport)
+                startActivity(intent)
             }
 
             override fun onDelete(cpf: String) {
@@ -49,8 +54,8 @@ class AlunosFragment : Fragment() {
                     .setTitle(getString(R.string.deleteAluno))
                     .setMessage(getString(R.string.textConfirmationDelete))
                     .setPositiveButton(getString(R.string.yes)) { dialog, which ->
-                     //  viewModel.delete(cpf)
-                      //  viewModel.getAll()
+                        viewModel.deleteAluno(cpf)
+                        viewModel.listAllAlunos()
                     }
                     .setNegativeButton(getString(R.string.cancel)) { dialog, which -> null }
                     .create()
@@ -58,22 +63,41 @@ class AlunosFragment : Fragment() {
             }
 
             override fun OnPayment(cpf: String) {
-             //  startActivity(Intent(context, Payment::class.java))
+                // startActivity(Intent(context, Payment::class.java))
             }
-
         }
+
 //      Responsável por passar o listener para o adapter
         adapter.attachListener(listener)
-//
-//        // Cria os observadores
-//        observe()
+
+        // Cria os observadores
+        observe()
 
         return binding.root
     }
 
+    // Observadores dos eventos da viewModel
+    private fun observe() {
+        viewModel.listAlunos.observe(viewLifecycleOwner) {
+            adapter.updateAlunos(it)
+        }
+        viewModel.deleteAluno.observe(viewLifecycleOwner) {
+            if (it.status())
+                Toast.makeText(context, R.string.textConfirmationDelete, Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(context, it.message(), Toast.LENGTH_SHORT).show()
+        }
+        viewModel.aluno.observe(viewLifecycleOwner) {
+            aluno = it
+        }
+        viewModel.alunoFailure.observe(viewLifecycleOwner){
+            if(!it.status()) Toast.makeText(context, it.message(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onResume() {
         super.onResume()
-        //  viewModel.list( )
+        viewModel.listAllAlunos()
     }
 
     private fun clickable() {
