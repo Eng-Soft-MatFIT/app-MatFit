2f26e3160319ac954db2cb2c93d148e5b9862593
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-07-30 20:08:44 -0300

    adicionado a annotation @SerializedName para os atributos

--- app/src/main/java/com/devmasterteam/tasks/service/model/Aluno.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/model/Aluno.kt
@@ -1,9 +1,21 @@
 package com.devmasterteam.tasks.service.model
 
+import com.google.gson.annotations.SerializedName
+import java.time.LocalDate
+
 data class Aluno(
+    @SerializedName("cpf")
     val cpf: String,
+
+    @SerializedName("nome")
     val name: String,
+
+    @SerializedName("esporte")
     val sport: String,
+
+    @SerializedName("dataPagamento")
     val datePayment: String,
-    val latePayment: Boolean = false
+
+    @SerializedName("pagamentoAtrasado")
+    val latePayment: Boolean = false,
 )
