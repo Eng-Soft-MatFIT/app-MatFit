eb5c68d4aecd49135f6156b26ce86a13f1c11f88
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-08-04 17:01:31 -0300

    Adicionado LOG para depuração de código na classe AlunoActivity

--- app/src/main/java/com/devmasterteam/tasks/view/ui/alunos/AddAlunoActivity.kt
+++ app/src/main/java/com/devmasterteam/tasks/view/ui/alunos/AddAlunoActivity.kt
@@ -1,13 +1,23 @@
 package com.devmasterteam.tasks.view.ui.alunos
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.databinding.ActivityAddAlunoBinding
 import com.devmasterteam.tasks.service.model.Aluno
 import com.devmasterteam.tasks.view.ui.viewmodel.AlunoViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.Thread.currentThread
 import java.time.LocalDate
 
 class AddAlunoActivity : AppCompatActivity() {
@@ -29,7 +39,7 @@ class AddAlunoActivity : AppCompatActivity() {
     }
 
     // Eventos de click dos botões
-    private fun clickable(){
+    private fun clickable() {
         binding.buttonAdd.setOnClickListener {
             addAluno()
         }
@@ -44,17 +54,23 @@ class AddAlunoActivity : AppCompatActivity() {
         val cpf = binding.editCpf.text.toString()
         val name = binding.editName.text.toString()
         val sport = binding.editSport.text.toString()
-        if (viewModel.saveAluno(applicationContext, Aluno(cpf, name, sport,"${LocalDate.now().plusMonths(1)}")))
-            finish()
-        else return
+        CoroutineScope(Dispatchers.IO).launch {
+            viewModel.save(Aluno(cpf, name, sport))
+            withContext(Dispatchers.Main){
+                viewModel.listAllAlunos()
+            }
+        }
     }
 
-    private fun observer(){
-        viewModel.saveAluno.observe(this){
-            if (it.status())
+    private fun observer() {
+        viewModel.saveAluno.observe(this) {
+            if (it.status()) {
+                Log.d("AddAlunoActivity", "Success: ${it.status()}")
                 Toast.makeText(applicationContext, R.string.textSucessRegister, Toast.LENGTH_SHORT).show()
-            else
+            } else {
+                Log.d("AddAlunoActivity", "Failure: ${it.status()}")
                 Toast.makeText(applicationContext, it.message(), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
