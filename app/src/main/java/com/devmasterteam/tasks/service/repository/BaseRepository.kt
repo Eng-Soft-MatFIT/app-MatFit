dacba9fc7ba1d9fdf539bbf5fcee1435b5720672
João Vitor Avelar <jvitoravelar1200@gmail.com>
2024-08-04 16:58:38 -0300

    Refatorado BaseRepository

--- app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/BaseRepository.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkCapabilities
 import android.os.Build
+import android.util.Log
 import com.devmasterteam.tasks.R
 import com.devmasterteam.tasks.service.constants.Constants
 import com.devmasterteam.tasks.service.listener.APIListener
+import com.devmasterteam.tasks.service.model.ErrorResponse
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,20 +18,38 @@ import retrofit2.Response
 open class BaseRepository(val context: Context) {
 
     private fun failResponse(str: String): String {
-        return Gson().fromJson(str, String::class.java)
+        return try {
+            val errorResponse = Gson().fromJson(str, ErrorResponse::class.java)
+            errorResponse.message ?: "Erro desconhecido"
+        } catch (e: JsonSyntaxException) {
+            "Erro de sintaxe JSON: ${e.localizedMessage}"
+        } catch (e: IllegalStateException) {
+            "Erro de estado JSON: ${e.localizedMessage}"
+        }
     }
 
     fun <T> executeCall(call: Call<T>, listener: APIListener<T>) {
         call.enqueue(object : Callback<T> {
             override fun onResponse(call: Call<T>, response: Response<T>) {
-                if (response.code() == Constants.HTTP.SUCCESS)
-                    response.body()?.let { listener.onSuccess(it) }
-                else
-                    listener.onFailure(failResponse(response.errorBody()!!.string()))
+                Log.d("BaseRepository", "onResponse: ${response.code()}")
+                if (response.isSuccessful)
+                    response.body()?.let {
+                        listener.onSuccess(it)
+                    } ?: run {
+                        listener.onFailure("Resposta vazia do servidor.")
+                    }
+                else {
+                    val errorBody = response.errorBody()?.string()
+                    if (errorBody != null)
+                        listener.onFailure(failResponse(errorBody))
+                    else
+                        listener.onFailure("Erro desconhecido.")
+                }
             }
 
             override fun onFailure(call: Call<T>, t: Throwable) {
-                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+                Log.d("BaseRepository", "onFailure: ${t.localizedMessage}")
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED) + " => ${t.localizedMessage}")
             }
         })
     }
@@ -57,4 +78,26 @@ open class BaseRepository(val context: Context) {
         }
         return result
     }
+
+    /*
+        private fun failResponse(str: String): String {
+        return Gson().fromJson(str, String::class.java)
+    }
+
+    fun <T> executeCall(call: Call<T>, listener: APIListener<T>) {
+        call.enqueue(object : Callback<T> {
+            override fun onResponse(call: Call<T>, response: Response<T>) {
+                if (response.code() == Constants.HTTP.SUCCESS)
+                    response.body()?.let { listener.onSuccess(it) }
+                else
+                    listener.onFailure(failResponse(response.errorBody()!!.string()))
+            }
+
+            override fun onFailure(call: Call<T>, t: Throwable) {
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+            }
+        })
+    }
+     */
+
 }
